613d94437e8c94f3d1e0a2c951153fd7515a8c3f
charlifrank <jayfrank0528@gmail.com>
2017-06-02 12:50:35 -0500

    (bugfix) Swipe through cards with correct words

--- src/pages/card-viewer/card-viewer.ts
+++ src/pages/card-viewer/card-viewer.ts
@@ -14,6 +14,9 @@ export class CardViewerPage {
   public rootPage: any = CardViewerPage;
   public profile: any;
   public deck: Array<any>;
+  public word: any;
+  public wordsLanguages: any;
+  public wordsTranslations: any;
 
   constructor(public navCtrl: NavController,
     public translateService: TranslateService,
@@ -26,10 +29,12 @@ export class CardViewerPage {
           this.profile = profile;
           translateService.use(languageService.translateLang(this.profile.nativeLang));
           this.deck = this.deckService.getCurrentDeck();
+          this.deck = this.deck[0].cards
           console.log('this.deck')
           console.log(JSON.stringify(this.deck))
           console.log(typeof this.deck[0])
           console.log('this.deck')
+          this.translations();
         })
         .catch(err => {
           console.log("Error" + JSON.stringify(err))
@@ -39,5 +44,55 @@ export class CardViewerPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad CardViewerPage');
   }
+  translations() {
+    console.log('inside translations')
+    this.wordsLanguages = Object.keys(JSON.parse(this.deck[0].wordMap))
+    this.wordsTranslations = JSON.parse(this.deck[0].wordMap)
+    
+    this.word = this.wordsTranslations[this.wordsLanguages[0]];
+    console.log("this.word")
+    console.log(this.word)
+    console.log("this.word")
+  }
+  swipeLeftEvent(index) {
+    console.log(index, 'swiped left')
+    console.log('swiped left')
+    if (index < this.deck.length - 1) {
+      let currentPos = index + 1
 
+      this.wordsLanguages = Object.keys(JSON.parse(this.deck[currentPos].wordMap))
+      this.wordsTranslations = JSON.parse(this.deck[currentPos].wordMap)
+      
+      this.word = this.wordsTranslations[this.wordsLanguages[0]];
+    }
+  }
+  swipeRightEvent(index) {
+    console.log(index, 'swiped right')
+    console.log('swiped right')
+    if (index > 0) {
+    let currentPos = index - 1;
+
+    this.wordsLanguages = Object.keys(JSON.parse(this.deck[currentPos].wordMap))
+    this.wordsTranslations = JSON.parse(this.deck[currentPos].wordMap)
+    
+    this.word = this.wordsTranslations[this.wordsLanguages[0]];
+    }
+  }
+  flip(index) {
+    console.log(index)
+    console.log("index")
+    console.log('flip')
+
+    if (this.word === this.wordsTranslations[this.wordsLanguages[0]]){
+    console.log('flip before 0', this.word)
+      this.word = this.wordsTranslations[this.wordsLanguages[1]];
+    console.log('flip after 0', this.word)
+      return;
+    } else if (this.word === this.wordsTranslations[this.wordsLanguages[1]]){
+    console.log('flip before 1', this.word)
+      this.word = this.wordsTranslations[this.wordsLanguages[0]];
+    console.log('flip after 1', this.word)
+    return;
+    }
+  }
 }
